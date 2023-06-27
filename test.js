ad6416c3f22d4962707ddeea7b4443cd8f9a8aa5
kawanoyasunori <kawano.yasunori@relation2012.com>
2023-06-27 10:33:50 +0900

    test

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ function test() {
   console.log('test')
   console.log('test')
   console.log('test')
+  console.log('test')
+  console.log('test')
   console.log('diff')
   console.log('diff')
   console.log('diff')
